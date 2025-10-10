93ead4e50084dab2f8147681b4e5716bd878ee4a
Prosta-Olya <siverolya@gmail.com>
2025-10-10 10:02:34 +0500

    Add files via upload

---
+++ pract21/app/src/main/java/com/bignerdranch/android/pract21/new_contact.kt
@@ -0,0 +1,11 @@
+package com.bignerdranch.android.pract21
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class new_contact : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_contact)
+    }
+}
