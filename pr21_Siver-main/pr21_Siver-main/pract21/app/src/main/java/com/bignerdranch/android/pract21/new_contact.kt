7d120b6b025020a64b9d273b6576b1d326b345e9
Prosta-Olya <siverolya@gmail.com>
2025-10-22 09:59:38 +0500

    Add files via upload

--- pr21_Siver-main/pr21_Siver-main/pract21/app/src/main/java/com/bignerdranch/android/pract21/new_contact.kt
+++ pr21_Siver-main/pr21_Siver-main/pract21/app/src/main/java/com/bignerdranch/android/pract21/new_contact.kt
@@ -1,11 +1,24 @@
 package com.bignerdranch.android.pract21
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class new_contact : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_contact)
+    }
+
+    fun back(view: View) {
+        startActivity(Intent(this,contacts::class.java))
+        finish()
+    }
+
+    fun enter(view: View) {
+
+        startActivity(Intent(this,contacts::class.java))
+        finish()
     }
 }
--- pr21_Siver-main/pract21/app/src/main/java/com/bignerdranch/android/pract21/contacts.kt
+++ pr21_Siver-main/pract21/app/src/main/java/com/bignerdranch/android/pract21/contacts.kt
@@ -1,32 +1,11 @@
 package com.bignerdranch.android.pract21
 
-import android.content.Intent
-import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
-import android.widget.EditText
-import android.widget.ListView
-
-private lateinit var list:ListView
-private lateinit var button2: Button
-private lateinit var button3: Button
-private lateinit var prefs: SharedPreferences
 
 class contacts : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contacts)
-    }
-
-    fun back(view: View) {
-        startActivity(Intent(this,MainActivity::class.java))
-        finish()
-    }
-
-    fun enter(view: View) {
-        startActivity(Intent(this,new_contact::class.java))
-        finish()
     }
 }
